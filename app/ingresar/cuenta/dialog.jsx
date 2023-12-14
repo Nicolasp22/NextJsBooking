52a59662a3036b434f96ad2a2589063a345fe7ba
Nicolas Pagola <nicolaspagola@outlook.com>
2023-12-14 19:13:38 -0300

    aa

---
+++ app/ingresar/cuenta/dialog.jsx
@@ -0,0 +1,10 @@
+export default function Dialog () {
+    const abrir = document.getElementsByClassName("productos");
+    const favDialog = document.getElementsById("favDialog")
+    favDialog.addEventListener("click", () =>  {
+
+})
+  return(
+    <></>
+)
+}
