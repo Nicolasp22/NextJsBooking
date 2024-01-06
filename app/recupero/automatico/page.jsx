19ea59ed7a118ea3dfb6d2a4e5ea087e2f2911be
Nicolas Pagola <nicolaspagola@outlook.com>
2024-01-06 16:58:03 -0300

    Responsive 3

--- app/recupero/automatico/page.jsx
+++ app/recupero/automatico/page.jsx
@@ -5,7 +5,7 @@ export default function Automatico () {
     return(
         <> 
          <div className="gracias"> 
-       <h2>En el caso de tener una cuenta activa con la casilla indicada se habrá enviado un correo automatico que contiene un Link en el que debe ingresar para acceder a su cuenta.
+       <h2>En caso de tener una cuenta activa con la casilla indicada se habrá enviado un correo automatico con un link al que debe ingresar para acceder a su cuenta. 
       </h2>
       <h3>Favor de no dar respuesta al mail automatico</h3>
       <Link href="../ingresar" className="link"><div className="texto">Ingresar</div></Link>
